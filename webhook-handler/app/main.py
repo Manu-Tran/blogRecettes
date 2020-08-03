9ddb493264a60a9e3a340ba229bd426f0da99f9c
Manu-Tran <emmanuel.tran@gmail.com>
2020-08-03 13:02:31 +0200

    FIX: Correct syntax to pull the repo

--- webhook-handler/app/main.py
+++ webhook-handler/app/main.py
@@ -5,18 +5,21 @@ import git
 app = Flask(__name__)  # Standard Flask app
 webhook = Webhook(app) # Defines '/postreceive' endpoint
 string = "Hello world"
-gitRepo = git.cmd.Git("/meta")
+gitRepo= git.Repo("/meta")
+o = gitRepo.remotes.origin
+gitRepo.heads.master.set_tracking_branch(o.refs.master)
+gitRepo.heads.master.checkout()
 
 @app.route("/")        # Standard Flask endpoint
 def hello_world():
-    gitRepo.pull()
+    o.pull()
     print("Git repo updated !")
     return(string)
 
 @webhook.hook()        # Defines a handler for the 'push' event
 def on_push(data):
     print("Got push with: {0}".format(data))
-    gitRepo.pull()
+    o.pull()
     print("Git repo updated !")
 
 
