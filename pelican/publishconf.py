5bff7fb4ec2e06bd1e969f7dfadc2ded8c3e73ed
Manu-Tran <emmanuel.tran@gmail.com>
2020-08-03 14:00:42 +0200

    FIX: Better volume management

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -11,7 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://recette.cgnan.fr'
+# SITEURL = 'http://recette.cgnan.fr'
+SITEURL = 'http://149.202.43.37'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
--- setupServer.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-git clone https://github.com/getpelican/pelican-plugins pelican/pelican-plugins
