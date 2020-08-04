a8261273951ea891b2217aaf907d30b6fd282e2b
Delmassad <32296734+Delmassad@users.noreply.github.com>
2020-08-04 11:22:28 +0200

    Update main.py
    PEP8

--- webhook-handler/app/main.py
+++ webhook-handler/app/main.py
@@ -2,25 +2,27 @@ from github_webhook import Webhook
 from flask import Flask
 import git
 
-app = Flask(__name__)  # Standard Flask app
-webhook = Webhook(app) # Defines '/postreceive' endpoint
-gitRepo= git.Repo("/meta")
+app = Flask(__name__)   # Standard Flask app
+webhook = Webhook(app)  # Defines '/postreceive' endpoint
+gitRepo = git.Repo("/meta")
 o = gitRepo.remotes.origin
 gitRepo.heads.master.set_tracking_branch(o.refs.master)
 gitRepo.heads.master.checkout()
 
-@app.route("/")        # Standard Flask endpoint
+
+@app.route("/")         # Standard Flask endpoint
 def on_get():
     o.pull()
     print("Git repo updated !")
-    return("Git repo updated !")
+    return "Git repo updated !"
 
-@webhook.hook()        # Defines a handler for the 'push' event
+
+@webhook.hook()         # Defines a handler for the 'push' event
 def on_push(data):
     print("Got push with: {0}".format(data))
     o.pull()
     print("Git repo updated !")
-    return("Git repo updated !")
+    return "Git repo updated !"
 
 
 if __name__ == "__main__":
